3c6e77bc423b3586d459ba3ae0dbf585c7e68768
rodwyn <rodwynmoreno@gmail.com>
2021-08-30 17:59:01 -0500

    Created category list.

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const GiftExpertApp = () => {
+  const [categories, setCategories] = useState(['Gorillaz', 'Deftones', 'The Chemical Brothers' ]);
+  const handleAdd = () => {
+    setCategories(['Radiohead', ...categories]);
+  };
+
   return (
     <>
       <h2>GiftExpertApp</h2>
       <hr />
+
+      <button onClick={handleAdd}>Agregar</button>
+
+      <ol>
+        {
+          categories.map(category => {
+            return <li key={category}>
+              {category}
+            </li>
+          })
+        }
+      </ol>
     </>
   );
 };
