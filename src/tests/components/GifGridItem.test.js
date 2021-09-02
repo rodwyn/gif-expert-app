f9622dc5bf17438c257fe3cc8fe3819167072719
rodwyn <rodwynmoreno@gmail.com>
2021-09-02 18:39:07 -0500

    Implemented prop-types. Updated snapshots.

--- src/tests/components/GifGridItem.test.js
+++ src/tests/components/GifGridItem.test.js
@@ -3,8 +3,11 @@ import {shallow} from 'enzyme';
 import {GifGridItem} from '../../components/GifGridItem';
 
 describe('Testing <GifGridItem> component.', ()=> {
+  const title = 'my title';
+  const url = 'https://rodwyn.com/icon.jpg';
+
   test('Should render the component correctly.', ()=>{
-    const wrapper = shallow(<GifGridItem />);
+    const wrapper = shallow(<GifGridItem title={title} url={url} />);
     expect(wrapper).toMatchSnapshot();
   });
 });
