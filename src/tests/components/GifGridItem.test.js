ebd6a85de8897b4dd1c6c58fc604839f672f7665
rodwyn <rodwynmoreno@gmail.com>
2021-09-02 19:01:09 -0500

    Added component tests.

--- src/tests/components/GifGridItem.test.js
+++ src/tests/components/GifGridItem.test.js
@@ -5,9 +5,29 @@ import {GifGridItem} from '../../components/GifGridItem';
 describe('Testing <GifGridItem> component.', ()=> {
   const title = 'my title';
   const url = 'https://rodwyn.com/icon.jpg';
+  const wrapper = shallow(<GifGridItem title={title} url={url} />);
+  const image = wrapper.find('img');
 
   test('Should render the component correctly.', ()=>{
-    const wrapper = shallow(<GifGridItem title={title} url={url} />);
     expect(wrapper).toMatchSnapshot();
+  });
+
+  test('Should paragraph contains a correct title.', () => {
+    const paragraph = wrapper.find('p');
+    expect(paragraph.text().trim()).toBe(title);
+  });
+
+  test('Should have the same src vale.', () => {
+    expect(image.prop('src')).toBe(url);
+  })
+
+  test('Should have the same alt title.', () => {
+    expect(image.prop('alt')).toBe(title);
+  });
+
+
+  test('Should have the class name animate__bounceIn.', ()=>{
+    const div = wrapper.find('div');
+    expect(div.prop('className')).toContain('animate__bounceIn');
   });
 });
